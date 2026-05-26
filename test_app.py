a844361725af9ff11af190f832f01b6bf18942c4
Vlad <zukovvlad1111@mail.ru>
2026-05-26 13:52:11 +0400

    Force re-upload to ensure test runs

--- test_app.py
+++ test_app.py
@@ -1,14 +1,10 @@
 import os
 
 def test_project():
-    # Проверяем, существует ли файл index.html
-    assert os.path.exists("index.html"), "Ошибка: файл index.html не найден!"
-    
-    # Проверяем, что внутри файла есть нужный текст
     with open("index.html", "r", encoding="utf-8") as f:
         content = f.read()
-    assert "Моя лаборатория CI/CD" in content, "Ошибка: неверный заголовок сайта!"
-    
+    # Скрипт ТРЕБУЕТ наличия этой фразы
+    assert "Моя лаборатория CI/CD" in content, "ОШИБКА: Заголовок не найден!"
     print("Все тесты успешно пройдены!")
 
 if __name__ == "__main__":
